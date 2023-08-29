e7108ad8a4d0610381effce168dbf6dd7556b7a7
tmohn <tmohn@cvent.com>
2023-08-28 20:10:01 -0400

    upgrade cypress to newest version and add axe

---
+++ assumptionsAndEstimates.md
@@ -0,0 +1,13 @@
+## Assumptions
+    - unit tests exists for base functionality
+    - code will always compile
+    - tests should be able to run independently
+    - application is stable
+
+## Time estimates
+Total 3.5 hours
+  1 hour for requirement gathering and test stategey
+  2 hours of creating test suites
+  .5 hours of validating tests and examples
+  
+
---
+++ client/cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    apiUrl: "http://localhost:3001",
+  },
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
